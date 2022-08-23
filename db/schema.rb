61d2fea549e97dbe42e562799170f5e57f9868d4
SionaMarlin <snmarlin@gmail.com>
2022-08-23 15:58:23 +0200

    added resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_110300) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_105745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,10 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_110300) do
     t.integer "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "dog_id"
-    t.index ["dog_id"], name: "index_bookings_on_dog_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "dogs", force: :cascade do |t|
