d7cc014e7a1d5ea2031859b3fb91c7e9897144d8
sonja ladenstein <sonja.ladenstein@gmx.at>
2022-08-24 15:18:19 +0200

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :dogs, only: [:index, :show, :new, :create]
-  resources :bookings, only: [:new, :show, :create]
-
+  resources :dogs, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :show, :create]
+  end
 end
