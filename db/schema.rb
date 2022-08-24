2d4e3f948526164fee4cb1e460e8b5fd4d3f1628
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-08-24 13:11:42 +0200

    photo upload for dogs completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_08_24_092505) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_160124) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
