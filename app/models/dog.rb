60529a654de6a13ccee34ce6f5bf60b3e0a8f304
sonja ladenstein <sonja.ladenstein@gmx.at>
2022-08-25 11:26:20 +0200

    small changes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,4 @@
 class Dog < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
+  has_many :bookings
 end
