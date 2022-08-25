ddb90742f0b4a15d729f98c879412d08eaf93221
sonja ladenstein <sonja.ladenstein@gmx.at>
2022-08-25 10:41:08 +0200

    doggycards

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,8 @@
 class BookingsController < ApplicationController
+
   def create
     @dog = Dog.find(params[:dog_id])
     @booking = Booking.new(bookings_params)
-
     @booking.start_date = Date.new(params[:booking]["start_date(1i)"].to_i, params[:booking]["start_date(2i)"].to_i, params[:booking]["start_date(3i)"].to_i)
     @booking.end_date = Date.new(params[:booking]["end_date(1i)"].to_i, params[:booking]["end_date(2i)"].to_i, params[:booking]["end_date(3i)"].to_i)
     @booking.dog = @dog
@@ -35,3 +35,13 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:start_date, :end_date)
   end
 end
+
+
+# @list = List.find(params[:list_id])
+#     @bookmark = Bookmark.new(bookmark_params)
+#     @bookmark.list = @list
+#     if @bookmark.save
+#       redirect_to list_path(@list)
+#     else
+#       render 'new', status: :unprocessable_entity
+#     end
